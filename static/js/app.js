c1b07d9bfed901d5aae2b0ef9766e663666a46c2
caleighteahan <caleighteahan@gmail.com>
2023-07-27 21:17:13 -0700

    the rest of it

--- static/js/app.js
+++ static/js/app.js
@@ -79,4 +79,94 @@ function init() {
   };
   
  
+function createScatter(bubble) {
   
+    
+    d3.json(url).then((data) => {
+        
+        
+        let sampleInfo = data.samples;
+  
+        
+        let value = sampleInfo.filter(result => result.id == bubble);
+  
+         
+        let valueData = value[0];
+  
+        
+        let otu_ids = valueData.otu_ids;
+        let otu_labels = valueData.otu_labels;
+        let sample_values = valueData.sample_values;
+  
+        
+        console.log(otu_ids,otu_labels,sample_values);
+        
+        
+        let trace1 = {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Earth"
+            }
+        };
+        
+        let layout = {
+          title: "OTU ID",
+        };
+  
+          
+          Plotly.newPlot("bubble", [trace1], layout)
+      });
+  };
+  
+  
+function createSummary(summary) {
+  
+    
+    d3.json(url).then((data) => {
+  
+        
+        let metadata = data.metadata;
+  
+        
+        let value = metadata.filter(result => result.id == summary);
+  
+         
+        console.log(value)
+  
+         
+        let valueData = value[0];
+  
+        
+        d3.select("#sample-metadata").html("");
+  
+        
+        Object.entries(valueData).forEach(([key,value]) => {
+  
+             
+            console.log(key,value);
+  
+            d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`);
+        });
+    });
+  
+  };
+  
+  
+function optionChanged(newValue) { 
+  
+    
+    console.log(newValue); 
+  
+    
+    createScatter(newValue)
+    createBar(newValue)
+    createSummary(newValue)
+  };
+  
+  
+init();  
